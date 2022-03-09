1ae6539d7a5f99abdef8ef35ed8fb547fee47190
yichen_0403 <104032good@gmail.comm>
2022-03-09 21:14:33 +0800

    finished

---
+++ chat.py
@@ -0,0 +1,31 @@
+def read_file(filename):   #讀取檔案
+    lines = []
+    with open(filename, 'r', encoding='utf-8-sig')as f: #怪符號用-sig
+        for line in f:
+            lines.append(line.strip('\n'))
+    return lines
+
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:   #如果person有值，才做下面這一段
+            new.append(person + ': ' + line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w')as f:
+        for line in lines:
+            f.write(line + '\n')
+
+def main():
+    lines = read_file('input.txt')  #檔名用參數較好
+    lines = convert(lines)
+    write_file('output.txt', lines)
+main()
